a11e8a9a6c24b696465bdaca32e0a799bd35a265
Alberich LLerenas <allerenas4@ucol.mx>
2020-11-26 16:22:25 -0600

    function cicular coche

--- src/main.js
+++ src/main.js
@@ -27,6 +27,111 @@ export default class App {
         }
         return resultado;
     }
+    puedeCircular(dia, terminacion){
+        let flag;
+        switch(dia){
+            case 1:
+                if(terminacion == 0){
+                    flag = true;
+                }
+                else if(terminacion == 1){
+                    flag = false;
+                }
+                else if(terminacion == 2){
+                    flag = false;
+                }
+                else{
+                    flag = false;
+                }
+            break;
+            case 2:
+                if(terminacion == 0){
+                    flag = false;
+                }
+                else if(terminacion == 1){
+                    flag = true;
+                }
+                else if(terminacion == 2){
+                    flag = false;
+                }
+                else{
+                    flag = false;
+                }
+            break;
+            case 3:
+                if(terminacion == 0){
+                    flag = false;
+                }
+                else if(terminacion == 1){
+                    flag = false;
+                }
+                else if(terminacion == 2){
+                    flag = true;
+                }
+                else{
+                    flag = false;
+                }
+            break;
+            case 4:
+                if(terminacion == 0){
+                    flag = false;
+                }
+                else if(terminacion == 1){
+                    flag = false;
+                }
+                else if(terminacion == 2){
+                    flag = true;
+                }
+                else{
+                    flag = false;
+                }
+            break;
+            case 5:
+                if(terminacion == 0){
+                    flag = false;
+                }
+                else if(terminacion == 1){
+                    flag = false;
+                }
+                else if(terminacion == 2){
+                    flag = false;
+                }
+                else{
+                    flag = false;
+                }
+            break;
+            case 6:
+                if(terminacion == 0){
+                    flag = false;
+                }
+                else if(terminacion == 1){
+                    flag = false;
+                }
+                else if(terminacion == 2){
+                    flag = false;
+                }
+                else{
+                    flag = false;
+                }
+            break;
+            case 7:
+                if(terminacion == 0){
+                    flag = false;
+                }
+                else if(terminacion == 1){
+                    flag = false;
+                }
+                else if(terminacion == 2){
+                    flag = false;
+                }
+                else{
+                    flag = false;
+                }
+            break;
+            default: flag = false;
+        }
+        return flag;
+    }
 }
 
 let app = new App();
@@ -42,3 +147,12 @@ console.log("Kilometros: 80  Pesos",app.costoRenta(80));
 console.log("Kilometros: 150 Pesos:",app.costoRenta(150));
 console.log("Kilometros: 200 Pesos:",app.costoRenta(200));
 console.log("");
+
+console.log("Funcion que dias se pueden circular dependiendo terminacion placa: ");
+console.log("Lunes terminacion placa: 0",app.puedeCircular(1,0));
+console.log("Martes terminacion placa: 1",app.puedeCircular(2,1));
+console.log("Miercoles terminacion placa: 2",app.puedeCircular(3,2));
+console.log("Jueves terminacion placa: 2",app.puedeCircular(4,2));
+console.log("Viernes terminacion placa: 3",app.puedeCircular(5,3));
+console.log("Sabado terminacion placa: 1",app.puedeCircular(6,1));
+console.log("Domingo terminacion placa: 2",app.puedeCircular(7,2));
