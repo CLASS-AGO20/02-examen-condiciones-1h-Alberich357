70897b84637d646ec99641b71be96dd2b53b2693
Alberich LLerenas <allerenas4@ucol.mx>
2020-11-26 15:55:52 -0600

    function convertirPesos

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
+const DOLAR = 20.5;
+const EURO = 22.7;
 export default class App {
-
+    convertir(pesos){
+        let resultado;
+        if(pesos<2000){
+            resultado = pesos/DOLAR;
+        }
+        else if(pesos >= 2000){
+            resultado = pesos/EURO;
+        }
+        return resultado;
+    }
 }
 
 let app = new App();
+
+console.log("Funcion convertir pesos: ");
+console.log(app.convertir(1300),"Dolares");
+console.log(app.convertir(2500),"Euros");
+console.log("");
